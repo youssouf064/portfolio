e8aca6cd2ca4be1513fc116c1adb956d535af34b
Youssouf Seme <youssoufseme064@gmail.com>
2026-06-13 05:10:18 +0000

    Mise à jour du portfolio

--- app.py
+++ app.py
@@ -35,4 +35,5 @@ def cv():
     return render_template("cv.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
