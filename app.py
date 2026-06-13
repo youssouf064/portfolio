36820ded2628236095ea3a51a7a4e4cf82b37b34
Youssouf Seme <youssoufseme064@gmail.com>
2026-06-13 04:01:34 +0000

    Premier portfolio professionnel

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/skills")
+def skills():
+    return render_template("skills.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/experience")
+def experience():
+    return render_template("experience.html")
+
+@app.route("/formations")
+def formations():
+    return render_template("formations.html")
+
+@app.route("/cv")
+def cv():
+    return render_template("cv.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
